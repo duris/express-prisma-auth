09de0f2caf798fa4de6283aa428aba08a1a20282
Ross <ross@duris.io>
2023-03-09 12:54:04 -0500

    Added email sending to api

--- src/script.ts
+++ src/script.ts
@@ -3,6 +3,8 @@ import { PrismaClient } from "@prisma/client";
 const bcrypt = require("bcrypt");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv").config();
 
 const app = express();
 app.use(cors());
@@ -12,7 +14,7 @@ const prisma = new PrismaClient();
 
 app.get("/", async (req: Request, res: Response) => {
   const users = await prisma.user.findMany();
-  res.send("hello world");
+  res.send(users);
 });
 
 app.post("/signup", async (req: Request, res: Response) => {
@@ -26,8 +28,8 @@ app.post("/signup", async (req: Request, res: Response) => {
         password: hash,
       },
     })
-    .then(() => res.status(200).send(`Thanks for signing up, ${name}!`))
-    .catch((err) => res.status(400).json("Email exist already"));
+    .then(() => res.status(200).send(`Thanks for signing up, ${name}!`)) //TODO: Send Welcome Email
+    .catch((err) => res.status(400).json("Unable to register"));
 });
 
 app.post("/login", async (req: Request, res: Response) => {
@@ -41,11 +43,14 @@ app.post("/login", async (req: Request, res: Response) => {
     const isValid = await bcrypt.compare(password, user.password);
 
     if (isValid) {
-      // Set Session:
-      // Cookie
+      // TODO :
+      // Cookies +
       // JWT
-      const token = jwt.sign(user, "shhhhh");
+
+      // Create Token
+      const token = jwt.sign(user, process.env.PRIVATE_KEY);
       console.log(token);
+
       res.status(200).send(`Welcome back, ${user.name}!`);
     } else {
       res.status(400).send("Cannot login with username/password");
@@ -53,6 +58,36 @@ app.post("/login", async (req: Request, res: Response) => {
   } else {
     res.status(400).send("Cannot login with username/password");
   }
+});
+
+app.post("/email", async (req: Request, res: Response) => {
+  const { email, subject, text } = req.body;
+  const mailData = {
+    from: process.env.EMAIL_ADDRESS,
+    to: email,
+    subject: subject,
+    text: text,
+    html: `<b>Welcome ${email}! This is your dashboard.</b>
+             <br>Feel free to customize the look!<br/>`,
+  };
+
+  const transporter = nodemailer.createTransport({
+    port: process.env.EMAIL_PORT,
+    host: process.env.EMAIL_HOST,
+    auth: {
+      user: process.env.EMAIL_ADDRESS,
+      pass: process.env.EMAIL_KEY,
+    },
+    secure: true,
+  });
+
+  await transporter.sendMail(mailData, (error: Error) => {
+    if (error) {
+      return console.log(error);
+    }
+
+    res.status(200).send("Mail sent!");
+  });
 });
 
 async function main() {
